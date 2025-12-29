c298d73e3a0797da17e570885339e6f6d3df308c
Sahil Jindal <sahiljindal.thebest@gmail.com>
2025-12-29 19:03:59 +0000

    Limiting logic to AOC 2019

--- src/lib.rs
+++ src/lib.rs
@@ -23,56 +23,7 @@ library!(util "Utility modules to handle common recurring Advent of Code pattern
     ansi, bitset, grid, hash, heap, integer, iter, math, md5, parse, point, slice, thread
 );
 
-library!(year2015 "Help Santa by solving puzzles to fix the weather machine's snow function."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2016 "Defeat the Easter Bunny to save Christmas."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2017 "A technical support callout from the Elves escalates rapidly."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2018 "Travel through time to restore the festive timeline."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
 library!(year2019 "Rescue Santa from deep space with a solar system voyage."
     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25, intcode
-);
-
-library!(year2020 "What could go wrong trying to enjoy a well-deserved vacation?"
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2021 "Retrieve the keys to Santa's sleigh with an underwater submarine adventure."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2022 "Assist the Elves on their annual jungle expedition."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2023 "Restore global snow production."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2024 "Locate the Chief Historian in time for the big Christmas sleigh launch."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-library!(year2025 "Finish the North Pole decorations in time for Christmas."
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12
 );
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use std::env::args;
 use std::fs::read_to_string;
 use std::time::{Duration, Instant};
 
+const YEAR: u32 = 2019;
+
 struct Solution {
-    year: u32,
     day: u32,
     wrapper: fn(&str) -> (String, String),
 }
@@ -13,28 +14,11 @@ struct Solution {
 fn main() {
     // Parse command line options
     let mut iter = args().flat_map(|arg| arg.iter_unsigned().collect::<Vec<u32>>());
-    let (year, day) = (iter.next(), iter.next());
+    let day = iter.next();
 
-    // Build list of all solutions.
-    let solutions = [
-        year2015(),
-        year2016(),
-        year2017(),
-        year2018(),
-        year2019(),
-        year2020(),
-        year2021(),
-        year2022(),
-        year2023(),
-        year2024(),
-        year2025(),
-    ];
 
     // Run selected solutions.
-    let (stars, duration) = solutions
-        .iter()
-        .flatten()
-        .filter(|s| year.is_none_or(|y| y == s.year))
+    let (stars, duration) = year2019().iter()
         .filter(|s| day.is_none_or(|d| d == s.day))
         .fold((0, Duration::ZERO), run_solution);
 
@@ -46,21 +30,21 @@ fn main() {
 }
 
 fn run_solution((stars, duration): (u32, Duration), solution: &Solution) -> (u32, Duration) {
-    let Solution { year, day, wrapper } = solution;
-    let path = format!("input/year{year}/day{day:02}.txt");
+    let Solution { day, wrapper } = solution;
+    let path = format!("input/day{day:02}.txt");
 
     if let Ok(data) = read_to_string(&path) {
         let instant = Instant::now();
         let (part1, part2) = wrapper(&data);
         let elapsed = instant.elapsed();
 
-        println!("{BOLD}{YELLOW}{year} Day {day}{RESET}");
+        println!("{BOLD}{YELLOW}{YEAR} Day {day}{RESET}");
         println!("    Part 1: {part1}");
         println!("    Part 2: {part2}");
 
         (stars + 2, duration + elapsed)
     } else {
-        eprintln!("{BOLD}{RED}{year} Day {day}{RESET}");
+        eprintln!("{BOLD}{RED}{YEAR} Day {day}{RESET}");
         eprintln!("    Missing input!");
         eprintln!("    Place input file in {BOLD}{WHITE}{path}{RESET}");
 
@@ -73,7 +57,6 @@ macro_rules! run {
         fn $year() -> Vec<Solution> {
             vec![$(
                 Solution {
-                    year: stringify!($year).unsigned(),
                     day: stringify!($day).unsigned(),
                     wrapper: |data: &str| {
                         use aoc::$year::$day::*;
@@ -90,56 +73,7 @@ macro_rules! run {
     }
 }
 
-run!(year2015
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2016
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2017
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2018
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
 run!(year2019
     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2020
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2021
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2022
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2023
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2024
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2025
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12
 );
